0785e21f1433d58549e0438ae4f44c89df55e612
sakogaya <122752678+sakogaya@users.noreply.github.com>
2023-02-25 09:50:52 +0000

    Update test.js

--- test.js
+++ test.js
@@ -200,18 +200,6 @@ function actualizarTotalCarrito(){
 
 
 
-        $(document).ready(function()
-        {
-        $('#containerSlider').slick({
-            dots: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            });
-        });
-    
 
 
 
